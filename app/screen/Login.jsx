db7a80421abc5f3c1a8252f470833c85ffcf03e7
KPUN <62070067@kmitl.ac.th>
2022-10-19 20:07:20 +0700

    create login file

---
+++ app/screen/Login.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const Login = () => {
+    return (
+        <View>
+            <Text>Login Screen</Text>
+        </View>
+    )
+}
+
+export default Login
