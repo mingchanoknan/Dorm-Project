e80ecaa9819732a7178384b1a9d2f20b86c3e2ad
mingchanoknan <63070027@kmitl.ac.th>
2022-10-19 20:41:12 +0700

    create all tenant screen

---
+++ app/screen/MangeAccount.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const MangeAccount = () => {
+    return (
+        <View>
+            <Text>Annouce News Screen for develop</Text>
+        </View>
+    )
+}
+
+export default MangeAccount
---
+++ app/screen/tenant/AnnouceNews.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const AnnouceNews = () => {
+    return (
+        <View>
+            <Text>Annouce News Screen for develop</Text>
+        </View>
+    )
+}
+
+export default AnnouceNews
