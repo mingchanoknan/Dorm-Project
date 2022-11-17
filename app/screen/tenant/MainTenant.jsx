62bc0b74e87633bba90e86d4102a888b83ed51f2
pxpleng <63070182@kmitl.ac.th>
2022-11-18 05:49:23 +0700

    connect back with news

--- app/screen/tenant/MainTenant.jsx
+++ app/screen/tenant/MainTenant.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   View,
@@ -13,7 +13,7 @@ import {
 import { Ionicons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5 } from "@expo/vector-icons";
 import { Card, Layout, Divider } from "@ui-kitten/components";
 import RoomGridTile from "../../component/contract/RoomGridTile";
 import { RENT } from "../../dummy/RENT";
@@ -21,100 +21,123 @@ import { FontAwesome } from "@expo/vector-icons";
 import { NEWS } from "../../dummy/NEWS";
 import News from "../../component/annoucenews/news";
 import AnnouceNews from "./AnnouceNews";
+import { baseUrl } from "@env";
+import axios from "axios";
 
 const MainTenant = ({ route, navigation }) => {
-  const annNews = NEWS;
-  const [text, setText] = React.useState("");
+  const [news, setNews] = useState(null);
 
   const renderGridItem = (itemData) => {
     console.log(itemData);
     return (
       <News
+      numberOfLines={3}
         item={itemData}
         width={"90%"}
+        canEdit={false}
         onSelect={() => {
-          navigation.navigate("NewsDetail",{
-            title : itemData.item.title,
+          navigation.navigate("NewsDetail", {
+            title: itemData.item.title,
             newsId: itemData.item.id,
-            data : itemData.item
+            data: itemData.item,
           });
         }}
       />
     );
   };
 
+  useEffect(() => {
+    const url = `${baseUrl}/news`;
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(url);
+        if (response.status === 200) {
+          setNews(response.data);
+          console.log(response.data);
+          return;
+        } else {
+          throw new Error("Failed");
+        }
+      } catch (error) {
+        console.log("error");
+      }
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <View style={styles.view}>
-        <Image
+      <Image
         source={require("../../assets/bg_login.jpg")}
         style={styles.background}
       ></Image>
       <View style={[styles.container]}>
+        <View
+          style={{
+            // backgroundColor: "red",
+            width: "100%",
+            height: "20%",
+            alignItems: "center",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <View
             style={{
-              // backgroundColor: "red",
-              width: "100%",
-              height: "20%",
+              width: "90%",
+              height: "100%",
+              borderRadius: "10%",
+              padding: 20,
+              elevation: 13,
+              flexDirection: "row",
+              justifyContent: "space-around",
+              alignSelf: "center",
               alignItems: "center",
-              justifyContent: "center",
-              alignItems: 'center'
             }}
           >
-            <View
-              style={{
-                width: "90%",
-                height: "100%",
-                borderRadius: "10%",
-                padding: 20,
-                elevation: 13,
-                flexDirection: "row",
-                justifyContent: "space-around",
-                alignSelf: "center",
-                alignItems: "center",
-              }}
-            >
-              <View style={styles.viewCircle}>
-                <TouchableOpacity
-                  style={styles.circle}
-                  onPress={() => navigation.navigate("InvoiceBill")}
-                >
-                  <FontAwesome5 name="file-invoice-dollar" size={36} color="white" />
-                  <Text style={styles.headTxt}>บิลค่าเช่า</Text>
-                </TouchableOpacity>
-               
-              </View>
+            <View style={styles.viewCircle}>
+              <TouchableOpacity
+                style={styles.circle}
+                onPress={() => navigation.navigate("InvoiceBill")}
+              >
+                <FontAwesome5
+                  name="file-invoice-dollar"
+                  size={36}
+                  color="white"
+                />
+                <Text style={styles.headTxt}>บิลค่าเช่า</Text>
+              </TouchableOpacity>
+            </View>
 
-              <View style={styles.viewCircle}>
-                <TouchableOpacity
-                  style={styles.circle}
-                  onPress={() => navigation.navigate("Parcel")}
-                >
-                  <FontAwesome5 name="box" size={36} color="white" />
-                  <Text style={styles.headTxt}>พัสดุ</Text>
-                </TouchableOpacity>
-                
-              </View>
+            <View style={styles.viewCircle}>
+              <TouchableOpacity
+                style={styles.circle}
+                onPress={() => navigation.navigate("Parcel")}
+              >
+                <FontAwesome5 name="box" size={36} color="white" />
+                <Text style={styles.headTxt}>พัสดุ</Text>
+              </TouchableOpacity>
+            </View>
 
-              <View style={styles.viewCircle}>
-                <TouchableOpacity
-                  style={styles.circle}
-                  onPress={() => navigation.navigate("Reports")}
-                >
-                  <Ionicons name="megaphone" size={36} color="white" />
-                  <Text style={styles.headTxt}>แจ้งเรื่อง</Text>
-                </TouchableOpacity>
-                
-              </View>
+            <View style={styles.viewCircle}>
+              <TouchableOpacity
+                style={styles.circle}
+                onPress={() => navigation.navigate("Reports")}
+              >
+                <Ionicons name="megaphone" size={36} color="white" />
+                <Text style={styles.headTxt}>แจ้งเรื่อง</Text>
+              </TouchableOpacity>
             </View>
           </View>
-              <FlatList
-              data={NEWS}
-              renderItem={renderGridItem}
-              numColumns={1}
-              keyExtractor={item => item.id}
-              navigation={navigation}
-            />
-            </View>
+        </View>
+        <FlatList
+          data={news}
+          renderItem={renderGridItem}
+          numColumns={1}
+          keyExtractor={(item) => item.id}
+          navigation={navigation}
+        />
+      </View>
     </View>
   );
 };
@@ -123,7 +146,7 @@ const styles = StyleSheet.create({
   view: {
     flex: 1,
     backgroundColor: "white",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   header: {
     margin: 10,
@@ -151,7 +174,7 @@ const styles = StyleSheet.create({
   container: {
     margin: 10,
     alignItems: "center",
-    flex: 1
+    flex: 1,
   },
   circle: {
     backgroundColor: "rgba(242, 247, 247, 0.5)",
@@ -162,14 +185,12 @@ const styles = StyleSheet.create({
     borderRadius: "20%",
     justifyContent: "center",
     alignItems: "center",
-    
   },
   viewCircle: {
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     alignSelf: "center",
-   
   },
   newsContent: {
     width: "100%",
@@ -215,7 +236,7 @@ const styles = StyleSheet.create({
     borderRadius: "50px",
     position: "absolute",
     zIndex: -100,
-    top: -150
+    top: -150,
   },
   input: {
     height: 60,
