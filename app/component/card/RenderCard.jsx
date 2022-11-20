a3aab9170901edaf166a5682b31aefed681bb9f7
mingchanoknan <63070027@kmitl.ac.th>
2022-11-20 22:40:50 +0700

    update Room detail

--- app/component/card/RenderCard.jsx
+++ app/component/card/RenderCard.jsx
@@ -1,14 +1,17 @@
 import { Alert, StyleSheet, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import RoomImageCarousel from "../carousel/imageCarousel";
-import { Input, Text, Card, Avatar,Divider } from "@ui-kitten/components";
+import { Input, Text, Card, Avatar, Divider } from "@ui-kitten/components";
 import { useState } from "react";
 
 const RenderCard = (props) => {
   const [commentInput, setCommentInput] = useState("");
   const [widthOfView, setWidthOfView] = useState(0);
   return (
-    <View style={styles.item}  onLayout={(event) => setWidthOfView(event.nativeEvent.layout.width)}>
+    <View
+      style={styles.item}
+      onLayout={(event) => setWidthOfView(event.nativeEvent.layout.width)}
+    >
       {/* Header */}
       <View
         style={{
@@ -17,8 +20,8 @@ const RenderCard = (props) => {
           flexDirection: "row",
           justifyContent: "space-between",
         }}
-          >
-              <View>
+      >
+        <View>
           <Text category="h5">{props.item.topic}</Text>
           <View
             style={{
@@ -36,9 +39,9 @@ const RenderCard = (props) => {
               ]}
             >
               <Text category="s2">{props.item.name} </Text>
-                      </View>
-                      
-                      <View
+            </View>
+
+            <View
               style={[
                 styles.writer,
                 { backgroundColor: "rgba(130, 219, 150, 0.6)" },
@@ -47,8 +50,8 @@ const RenderCard = (props) => {
               <Text category="s2">{props.item.room_number} </Text>
             </View>
           </View>
-              </View>
-              
+        </View>
+
         {props.page == "response" && props.item.status == false ? (
           <View
             style={{
@@ -110,107 +113,118 @@ const RenderCard = (props) => {
             </Text>
           </View>
         )}
-          </View>
-          <Divider />
-          <View style={{ flex: 1 }}>
-              <Text category="s1" style={{  textAlign: "center" ,marginTop:10}}>
+      </View>
+      <Divider />
+      <View style={{ flex: 1 }}>
+        <Text category="s1" style={{ textAlign: "center", marginTop: 10 }}>
           {props.item.content}
         </Text>
-        {props.item.image.length > 0 &&
-          <RoomImageCarousel image={props.item.image} width={widthOfView - 50} height={300} paginationColor={"#84E6E3" } />
-       }
-          
-       
-        <View style={{ display: "flex", alignItems: "flex-end",marginTop:10}}>
+        {props.item.image.length > 0 && (
+          <RoomImageCarousel
+            image={props.item.image}
+            width={widthOfView - 50}
+            height={300}
+            paginationColor={"#84E6E3"}
+          />
+        )}
+
+        <View
+          style={{ display: "flex", alignItems: "flex-end", marginTop: 10 }}
+        >
           <Text category="label" style={{ color: "#A4A2A2" }}>
             {props.item.date}
           </Text>
         </View>
-          </View>
-          <Divider />
-          {/* footer */}
-          <View style={{ margin: 10 }}>
-          <View
+      </View>
+      <Divider />
+      {/* footer */}
+      <View style={{ margin: 10 }}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <Input
+            style={{ minWidth: "70%", borderRadius: "25%" }}
+            multiline={true}
+            placeholder="Response Problem..."
+            onChangeText={(text) => setCommentInput(text)}
+            value={commentInput}
+          ></Input>
+          <TouchableOpacity
             style={{
+              backgroundColor: "rgba(249, 169, 145, 0.6)",
+              padding: 10,
               display: "flex",
-              flexDirection: "row",
+              alignItems: "center",
               justifyContent: "center",
+              borderRadius: "10%",
+              marginLeft: 20,
+            }}
+            onPress={() => {
+              if (commentInput != "") {
+                props.saveComment(props.item, commentInput);
+                setCommentInput("");
+              }
             }}
           >
-        <Input
-          style={{ minWidth: "70%", borderRadius: "25%" }}
-          multiline={true}
-          placeholder="Response Problem..."
-          onChangeText={(text) => setCommentInput(text)}
-          value={commentInput}
-                  ></Input>
-                   <TouchableOpacity
-              style={{
-                backgroundColor: "rgba(249, 169, 145, 0.6)",
-                padding: 10,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                borderRadius: "10%",
-                marginLeft: 20,
+            <Text category="label">SAVE</Text>
+          </TouchableOpacity>
+        </View>
+        {props.item.comments.length > 0 &&
+          props.item.comments.map((item, index) => (
+            <View key={index}>
+              <Card style={styles.card}>
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                  }}
+                >
+                  <Avatar
+                    style={{ marginRight: 10 }}
+                    size="large"
+                    source={require("../../assets/user.png")}
+                  />
+                  <View>
+                    <Text
+                      style={{ marginLeft: 20, marginBottom: 2 }}
+                      category="label"
+                    >
+                      {item.name}
+                    </Text>
+                    <View
+                      style={{
+                        backgroundColor: "white",
+                        paddingVertical: 5,
+                        paddingHorizontal: 15,
+                        minWidth: "60%",
+                        marginLeft: 10,
+                        borderRadius: 15,
+                        maxWidth: "85%",
+                        display: "flex",
                       }}
-                      onPress={() => {
-                          props.saveComment(props.item, commentInput);
-                      setCommentInput("")}}
-            >
-              <Text category="label">SAVE</Text>
-            </TouchableOpacity>
-              </View>
-              {props.item.comments.length > 0 &&
-           props.item.comments.map((item, index) => (
-              <View key={index}>
-                <Card style={styles.card}>
-                  <View
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                    }}
-                  >
-                    <Avatar
-                      style={{ marginRight: 10 }}
-                      size="large"
-                      source={require("../../assets/user.png")}
-                    />
-                    <View>
-                      <Text
-                        style={{ marginLeft: 20, marginBottom: 2 }}
-                        category="label"
-                      >
-                        {item.name}
-                      </Text>
-                      <View
-                        style={{
-                          backgroundColor: "white",
-                          paddingVertical: 5,
-                          paddingHorizontal: 15,
-                          minWidth: "60%",
-                          marginLeft: 10,
-                          borderRadius: 15,
-                          maxWidth: "85%",
-                          display: "flex",
-                        }}
-                      >
-                        <Text category="s1">{item.comment}</Text>
-                      </View>
+                    >
+                      <Text category="s1">{item.comment}</Text>
                     </View>
                   </View>
-                </Card>
-                <View style={{ display: "flex", alignItems: "flex-end" }}>
-                  <Text category="label" style={{ marginRight: 10,color:"#A4A2A2"}}>
-                    {item.date}
-                  </Text>
                 </View>
+              </Card>
+              <View style={{ display: "flex", alignItems: "flex-end" }}>
+                <Text
+                  category="label"
+                  style={{ marginRight: 10, color: "#A4A2A2" }}
+                >
+                  {item.date}
+                </Text>
               </View>
-            ))}
+            </View>
+          ))}
       </View>
     </View>
   );
-  
 };
 
 const styles = StyleSheet.create({
@@ -225,11 +239,11 @@ const styles = StyleSheet.create({
   },
   item: {
     borderRadius: "40%",
-      backgroundColor: "rgba(255, 255, 255, 0.80)",
-      marginTop: "2%",
-      marginHorizontal: "5%",
-      paddingTop: 10,
-      paddingHorizontal:20
+    backgroundColor: "rgba(255, 255, 255, 0.80)",
+    marginTop: "2%",
+    marginHorizontal: "5%",
+    paddingTop: 10,
+    paddingHorizontal: 20,
   },
   writer: {
     marginLeft: 10,
@@ -237,8 +251,8 @@ const styles = StyleSheet.create({
     borderRadius: "50%",
   },
   card: {
-      flex: 1,
-      marginTop: "2%",
+    flex: 1,
+    marginTop: "2%",
     borderColor: "rgba(144, 170, 203, 0)",
     backgroundColor: "rgba(103, 189, 252,0.3)",
     borderRadius: 20,
